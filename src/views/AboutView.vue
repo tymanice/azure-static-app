58924a794318a99dbbcad75dfdd22849d76356ab
Tyler Ruff <tymanice@gmail.com>
2024-06-09 22:51:51 -0400

    added get, post, and delete

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,51 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+let notes = ref([])
+let vNote = ref()
+
+const API_URL = 'http://localhost:5066/'
+
+const refreshData = async() => {
+  axios.get(API_URL + 'api/ToDoApp/GetNotes').then((response) => {
+    notes.value = response.data
+    console.log('DATA NEW', notes)
+  })
+}
+
+const addNewNotes = async() => {
+  const formData = new FormData();
+  formData.append("newNotes", vNote.value)
+  axios.post(API_URL + 'api/ToDoApp/AddNotes', formData).then(
+    (response)=>{
+      refreshData();
+      alert(response.data)
+  })
+}
+
+const deleteNotes = async(id) => {
+  axios.delete(API_URL + 'api/ToDoApp/DeleteNotes?id='+id).then(
+    (response)=>{
+      refreshData();
+      alert(response.data)
+  })
+}
+
+onMounted(async() => {
+  await refreshData()
+})
+</script>
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Todo</h1>
+    <input id="newNotes" v-model="vNote"/>
+    <button @click="addNewNotes()">Add Notes</button>
+    <p v-for="item in notes">
+      <b>{{ item.description }}</b>
+      <button @click="deleteNotes(item.id)">Delete Notes</button>
+    </p>
   </div>
 </template>
 
